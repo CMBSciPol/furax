2aca0302e588e20deefc09ddc244f70a66e3217d
Simon Biquard <sbiquard@gmail.com>
2024-04-18 12:13:23 +0100

    Add dedicated factory for HWPOperator

--- src/astrosim/operators/hwp.py
+++ src/astrosim/operators/hwp.py
@@ -1,5 +1,16 @@
+from jax import Array
+from jaxtyping import Float
+
+from astrosim.landscapes import ValidStokesType
 from astrosim.operators.qu_rotations import QURotationOperator
 
 
 class HWPOperator(QURotationOperator):
     """Operator for an ideal Half-wave plate."""
+
+    @classmethod
+    def create(
+        cls, shape: tuple[int, ...], stokes: ValidStokesType, hwp_angles: Float[Array, '...']
+    ):
+        # override to include additional factor 2 due to HWP properties
+        return super().create(shape, stokes, 2 * hwp_angles)

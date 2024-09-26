9930d4c570e75d0c26e43bfbc9e2f111287a594c
Simon Biquard <sbiquard+git@gmail.com>
2024-09-26 17:33:14 +0200

    Adress comments - GapFillingOperator not a child class of AbstractLinearOperator - simplify `get_kernel` function - random key splitting from detector names using the `split_key` method implemented for DetectorArray

--- src/furax/detectors.py
+++ src/furax/detectors.py
@@ -1,6 +1,9 @@
+from hashlib import sha1
+
 import jax
+import jax.numpy as jnp
 import numpy as np
-from jaxtyping import Float
+from jaxtyping import Float, PRNGKeyArray, Shaped
 
 
 class DetectorArray:
@@ -10,6 +13,7 @@ class DetectorArray:
         x: Float[np.ndarray, '*#dims'],  # type: ignore[type-arg]
         y: Float[np.ndarray, '*#dims'],  # type: ignore[type-arg]
         z: Float[np.ndarray | float, '*#dims'],  # type: ignore[type-arg]
+        names: list[str] | None = None,
     ) -> None:
         self.shape = np.broadcast(
             x, y, z
@@ -22,5 +26,26 @@ class DetectorArray:
         coords /= length
         self.coords = jax.device_put(coords)
 
+        # how many detectors there are
+        n_detectors = 1 if len(self.shape) == 1 else self.shape[-2]
+        if names is None:
+            # if not provided, create some detector names
+            names = [f'DET_{i}' for i in range(n_detectors)]
+        assert len(names) == n_detectors
+        self.names = names
+
     def __len__(self) -> int:
         return int(np.prod(self.shape))
+
+    def split_key(self, key: PRNGKeyArray) -> Shaped[PRNGKeyArray, ' _']:
+        """Folds the detector names in a random key to generate a key array."""
+        fold = jax.numpy.vectorize(jax.random.fold_in, signature='(),()->()')
+        det_hashes = [int(sha1(name.encode()).hexdigest(), 16) for name in self.names]
+        data = jnp.uint32([dh & 0xEFFFFFFF for dh in det_hashes])
+        subkeys: Shaped[PRNGKeyArray, '...'] = fold(key, data)
+        return subkeys
+
+
+class FakeDetectorArray(DetectorArray):
+    def __init__(self, shape: int | tuple[int, ...], names: list[str] | None = None) -> None:
+        super().__init__(np.zeros(shape), np.zeros(shape), 1.0, names)

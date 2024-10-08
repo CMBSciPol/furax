1047f65eeff0538dd87699e1110653844d6e2f77
Simon Biquard <sbiquard+git@gmail.com>
2024-10-08 17:50:28 +0200

    Fix key management in DetectorArray - handles multi-dimensional detector grid

--- src/furax/detectors.py
+++ src/furax/detectors.py
@@ -1,9 +1,10 @@
+from functools import reduce
 from hashlib import sha1
 
 import jax
 import jax.numpy as jnp
 import numpy as np
-from jaxtyping import Float, PRNGKeyArray, Shaped
+from jaxtyping import Array, Float, PRNGKeyArray, Shaped, UInt32
 
 
 class DetectorArray:
@@ -13,7 +14,6 @@ class DetectorArray:
         x: Float[np.ndarray, '*#dims'],  # type: ignore[type-arg]
         y: Float[np.ndarray, '*#dims'],  # type: ignore[type-arg]
         z: Float[np.ndarray | float, '*#dims'],  # type: ignore[type-arg]
-        names: list[str] | None = None,
     ) -> None:
         self.shape = np.broadcast(
             x, y, z
@@ -26,26 +26,29 @@ class DetectorArray:
         coords /= length
         self.coords = jax.device_put(coords)
 
-        # how many detectors there are
-        n_detectors = 1 if len(self.shape) == 1 else self.shape[-2]
-        if names is None:
-            # if not provided, create some detector names
-            names = [f'DET_{i}' for i in range(n_detectors)]
-        assert len(names) == n_detectors
+        # generate fake names for the detectors
+        # TODO(simon): accept user-defined names
+        det_indices = (_.astype(np.str_) for _ in np.indices(self.shape, sparse=True))
+        names = reduce(lambda a, b: a + '_' + b, det_indices, 'FAKE_DET')
         self.names = names
 
     def __len__(self) -> int:
         return int(np.prod(self.shape))
 
+    def _ids(self) -> UInt32[Array, '...']:
+        # vectorized hashing + converting to int + keeping only 7 bytes
+        name_to_int = np.vectorize(lambda s: int(sha1(s.encode()).hexdigest(), 16) & 0xEFFFFFFF)
+        # return detectors IDs as unsigned 32-bit integers
+        ids: UInt32[Array, '...'] = jnp.uint32(name_to_int(self.names))
+        return ids
+
     def split_key(self, key: PRNGKeyArray) -> Shaped[PRNGKeyArray, ' _']:
         """Folds the detector names in a random key to generate a key array."""
         fold = jax.numpy.vectorize(jax.random.fold_in, signature='(),()->()')
-        det_hashes = [int(sha1(name.encode()).hexdigest(), 16) for name in self.names]
-        data = jnp.uint32([dh & 0xEFFFFFFF for dh in det_hashes])
-        subkeys: Shaped[PRNGKeyArray, '...'] = fold(key, data)
+        subkeys: Shaped[PRNGKeyArray, '...'] = fold(key, self._ids())
         return subkeys
 
 
 class FakeDetectorArray(DetectorArray):
-    def __init__(self, shape: int | tuple[int, ...], names: list[str] | None = None) -> None:
-        super().__init__(np.zeros(shape), np.zeros(shape), 1.0, names)
+    def __init__(self, num: int | tuple[int, ...]) -> None:
+        super().__init__(np.zeros(num), np.zeros(num), 1.0)

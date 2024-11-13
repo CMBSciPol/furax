796eaec8805c30f666f9b23a18dd82b4a3b07607
Wassim KABALAN <wastondev@gmail.com>
2024-11-13 16:09:40 +0100

    add tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -7,7 +7,7 @@ import jax.numpy as jnp
 TEST_DATA = Path(__file__).parent / 'data'
 TEST_DATA_PLANCK = TEST_DATA / 'planck'
 TEST_DATA_SAT = TEST_DATA / 'sat'
-
+TEST_DATA_FGBUSTER = TEST_DATA / 'fgbuster'
 
 def arange(*shape: int, dtype=jnp.float32, start=1) -> jax.Array:
     return jnp.arange(start, prod(shape) + start, dtype=dtype).reshape(shape)

6732edac41c32b80b019f028301b3003b30e9145
Artem <artem.m.basyrov@gmail.com>
2025-02-06 10:48:20 +0100

    add beam operator

---
+++ src/furax/operators/beam_operator.py
@@ -0,0 +1,72 @@
+import jax
+import jax.numpy as jnp
+from furax._base.core import *
+import equinox
+import jax_healpy as jhp
+from jaxtyping import Array, Float
+from functools import partial
+
+@symmetric
+class BeamOperator(AbstractLinearOperator):
+    fwhm: float = equinox.field(static=True)
+    lmax: int = equinox.field(static=True)
+    _in_structure: PyTree[jax.ShapeDtypeStruct] = equinox.field(static=True)
+    _beam_fl: Float[Array, ' a'] = equinox.field(static=True)        
+    _inv_flag: bool = equinox.field(static=True)
+
+    def __init__(
+        self, 
+        fwhm: float, # [rad]
+        in_structure: PyTree[jax.ShapeDtypeStruct],
+        lmax = 2000,
+        inv_flag = False,
+    ) -> None:
+        self.fwhm = fwhm
+        self.lmax = lmax    
+        self._in_structure = in_structure
+        self._inv_flag = inv_flag
+
+        self._beam_fl = self._gauss_beam(self.fwhm, self.lmax) # to be replaced with jhp.sphtfunc.gauss_beam when it exists
+        if self._inv_flag:
+            self._beam_fl = 1/self._beam_fl
+
+
+    def _gauss_beam(self, fwhm, lmax=512, pol=False):
+        # just copying code from "https://github.com/healpy/healpy/blob/dd0506a4b51c2961bea50cd9e0361db0c634dfdb/lib/healpy/sphtfunc.py#L1235"
+        
+        sigma = fwhm / jnp.sqrt(8.0 * jnp.log(2.0))
+        ell = jnp.arange(lmax + 1)
+        sigma2 = sigma ** 2
+        g = jnp.exp(-0.5 * ell * (ell + 1) * sigma2)
+        
+        if not pol:  # temperature-only beam
+            return g
+        else:  # polarization beam
+            # polarization factors [1, 2 sigma^2, 2 sigma^2, sigma^2]
+            pol_factor = jnp.exp([0.0, 2 * sigma2, 2 * sigma2, sigma2])
+            return g[:, jnp.newaxis] * pol_factor
+
+
+    def mv(self, x: PyTree[Inexact[Array, ' _a']]) -> PyTree[Inexact[Array, ' _b']]:
+        map2alm = partial(jhp.map2alm, iter=0, lmax=self.lmax, pol=False)
+        almxfl = partial(jhp.almxfl, fl=self._beam_fl, lmax=self.lmax)
+
+        def func(value):
+            nside = jhp.npix2nside(value.shape[-1])
+            alm2map = partial(jhp.alm2map, nside=nside, lmax=self.lmax, pol=False)
+
+            alm = jax.vmap(map2alm)(jnp.atleast_2d(value))
+            x = jax.vmap(almxfl)(jnp.atleast_2d(alm))
+            mp = jax.vmap(alm2map)(jnp.atleast_2d(x))
+
+            return mp.real
+
+        return jax.tree.map(func, x)
+
+
+    def in_structure(self) -> PyTree[jax.ShapeDtypeStruct]:
+            return self._in_structure
+
+
+    def inverse(self) -> AbstractLinearOperator:
+        return BeamOperator(fwhm=self.fwhm, in_structure=self._in_structure, lmax = self.lmax, inv_flag=True)

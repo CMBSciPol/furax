857b3324e7de4712f866975a20698fd61d1ae354
ArtemBasyrov <artem.m.basyrov@gmail.com>
2025-06-06 10:59:58 +0200

    reduced number of parameters

--- src/furax/operators/beam_operator.py
+++ src/furax/operators/beam_operator.py
@@ -7,6 +7,9 @@ from jaxtyping import Array, Float, PyTree, Inexact
 from functools import partial
 import numpy as np
 
+import healpy as hp
+from furax.landscapes import StokesPyTree
+
 @symmetric
 class BeamOperator(AbstractLinearOperator):
     """
@@ -19,29 +22,24 @@ class BeamOperator(AbstractLinearOperator):
         _beam_fl (Float[Array, 'a']): Beam transfer function.
         _inv_flag (bool): Flag to indicate if the inverse beam should be used.
     """
-    fwhm: float = equinox.field(static=True)
+    #fwhm: float = equinox.field(static=True)
     lmax: int = equinox.field(static=True)
     _in_structure: PyTree[jax.ShapeDtypeStruct] = equinox.field(static=True)
     _beam_fl: Float[Array, ' a'] = equinox.field(static=True)        
-    _inv_flag: bool = equinox.field(static=True)
+    #_inv_flag: bool = equinox.field(static=True)
 
     def __init__(
         self, 
-        fwhm: float, # [rad]
         in_structure: PyTree[jax.ShapeDtypeStruct],
-        lmax: int = 512,
-        inv_flag: bool = False,
+        bl: Float[Array, 'a'],
+        lmax: int,
     ) -> None:
-        self.fwhm = fwhm
+        
         self.lmax = lmax    
         self._in_structure = in_structure
-        self._inv_flag = inv_flag
+        self._beam_fl = bl #self._gauss_beam(self.fwhm, self.lmax) # to be replaced with jhp.sphtfunc.gauss_beam when it exists
 
-        self._beam_fl = self._gauss_beam(self.fwhm, self.lmax) # to be replaced with jhp.sphtfunc.gauss_beam when it exists
-        if self._inv_flag:
-            self._beam_fl = 1/self._beam_fl
-
-
+    '''
     def _gauss_beam(self, fwhm: float, lmax: int = 512, pol: bool = False):
         # just copying code from "https://github.com/healpy/healpy/blob/dd0506a4b51c2961bea50cd9e0361db0c634dfdb/lib/healpy/sphtfunc.py#L1235"
         
@@ -56,11 +54,11 @@ class BeamOperator(AbstractLinearOperator):
             # polarization factors [1, 2 sigma^2, 2 sigma^2, sigma^2]
             pol_factor = np.exp([0.0, 2 * sigma2, 2 * sigma2, sigma2])
             return g[:, np.newaxis] * pol_factor
-
+    '''
 
     def mv(self, x: PyTree[Inexact[Array, ' _a']]) -> PyTree[Inexact[Array, ' _b']]:
-        map2alm = partial(jhp.map2alm, iter=3, lmax=self.lmax, pol=False)
-        almxfl = partial(jhp.almxfl, fl=self._beam_fl, lmax=self.lmax)
+        map2alm = partial(jhp.map2alm, iter=1, lmax=self.lmax, pol=False)
+        almxfl = partial(jhp.almxfl, fl=self._beam_fl)#, lmax=self.lmax)
 
         def func(value):
             nside = jhp.npix2nside(value.shape[-1])
@@ -69,9 +67,23 @@ class BeamOperator(AbstractLinearOperator):
             alm = jax.vmap(map2alm)(jnp.atleast_2d(value))
             x = jax.vmap(almxfl)(jnp.atleast_2d(alm))
             mp = jax.vmap(alm2map)(jnp.atleast_2d(x))
+            '''
+            mp = jnp.zeros_like(value)
+            for i in range(value.shape[0]):
+                alm = map2alm(value[i])
+                x = almxfl(alm)
+                mp.at[i].set(alm2map(x))
+            '''
 
             return mp.real
 
+        '''
+        xi = func(x.i)
+        xq = func(x.q)
+        xu = func(x.u)
+        
+        y = StokesPyTree.from_stokes(I=xi, Q=xq, U=xu)
+        '''
         return jax.tree.map(func, x)
 
 
@@ -80,4 +92,4 @@ class BeamOperator(AbstractLinearOperator):
 
 
     def inverse(self) -> AbstractLinearOperator:
-        return BeamOperator(fwhm=self.fwhm, in_structure=self._in_structure, lmax=self.lmax, inv_flag=True)
+        return BeamOperator(in_structure=self._in_structure, lmax=self.lmax, bl=1./self._beam_fl)

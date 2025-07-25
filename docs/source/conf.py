035a72a4fef7b314b834501565c8686677ac5b70
Pierre Chanial <chanial@apc.in2p3.fr>
2025-07-25 12:41:18 +0200

    Improve documentation configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,148 +3,120 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Furax'
-copyright = '2024, Furax Developers'
-author = 'Furax Developers'
+import os
+import sys
 
-# The full version, including alpha/beta/rc tags
-release = '0.1.0'
-version = '0.1'
+sys.path.insert(0, os.path.abspath('../../'))
+
+project = 'jax-healpy'
+copyright = '2024-2025, Furax contributors.'
+author = 'Furax contributors'
+
+# Get version from setuptools_scm
+try:
+    from setuptools_scm import get_version
+
+    release = get_version(root='../..')
+except Exception:
+    # Fallback if setuptools_scm fails
+    try:
+        from importlib.metadata import version
+
+        release = version('furax')
+    except Exception:
+        release = '0.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.githubpages',
     'myst_parser',
     'nbsphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
     'sphinx_copybutton',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
-
-# The suffix(es) of source filenames.
-source_suffix = {
-    '.rst': None,
-    '.md': None,
-}
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.
 html_theme = 'sphinx_book_theme'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-html_theme_options = {
-    'repository_url': 'https://github.com/your-org/furax',
-    'use_repository_button': True,
-    'use_issues_button': True,
-    'use_edit_page_button': True,
-    'use_download_button': True,
-    'launch_buttons': {
-        'notebook_interface': 'jupyterlab',
-        'binderhub_url': 'https://mybinder.org',
-    },
-}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
-
-# -- Extension configuration -------------------------------------------------
-
-# -- Options for autodoc ----------------------------------------------------
-autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__',
+# Theme options
+html_theme_options = {
+    'path_to_docs': 'docs/source',
+    'repository_url': 'https://github.com/CMBSciPol/furax',
+    'use_download_button': True,
+    'use_edit_page_button': True,
+    'use_issues_button': True,
+    'use_repository_button': True,
+    'use_sidenotes': True,
 }
 
-# Automatically extract typehints when specified and place them in
-# descriptions of the relevant function/method.
-autodoc_typehints = 'description'
-
-# Don't show class signature with the class' name.
-autodoc_class_signature = 'separated'
-
-# -- Options for autosummary ------------------------------------------------
-autosummary_generate = True
-
-# -- Options for napoleon ----------------------------------------------------
+# Napoleon settings
+napoleon_attr_annotations = True
 napoleon_google_docstring = True
-napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
+napoleon_numpy_docstring = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False
 napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
 
-# -- Options for intersphinx ------------------------------------------------
+# Intersphinx mapping
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
     'jax': ('https://jax.readthedocs.io/en/latest/', None),
-    'astropy': ('https://docs.astropy.org/en/stable/', None),
+    'healpy': ('https://healpy.readthedocs.io/en/latest/', None),
 }
 
-# -- Options for nbsphinx ---------------------------------------------------
-nbsphinx_execute = 'never'
-nbsphinx_allow_errors = True
-
-# -- Options for myst parser ------------------------------------------------
+# MyST parser configuration
 myst_enable_extensions = [
     'amsmath',
     'colon_fence',
     'deflist',
     'dollarmath',
+    'fieldlist',
     'html_admonition',
     'html_image',
     'replacements',
     'smartquotes',
-    'substitution',
+    'strikethrough',
+    'tasklist',
 ]
 
-# -- Setup function ---------------------------------------------------------
+# Nbsphinx configuration
+nbsphinx_execute = 'always'
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc={'figure.dpi': 96}",
+]
 
+# Autodoc configuration
+autodoc_member_order = 'bysource'
+autodoc_typehints = 'description'
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
 
-def setup(app):
-    """Setup hook for Sphinx."""
-    # Add custom CSS
-    app.add_css_file('custom.css')
-
-
-# Add the project root to the Python path so we can import furax
-sys.path.insert(0, os.path.abspath('../../src'))
+# Copy button configuration
+copybutton_prompt_text = r'>>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: '
+copybutton_prompt_is_regexp = True

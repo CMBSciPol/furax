987ef572f4cba6279f3d82061362887b7acb44e9
ArtemBasyrov <artem.m.basyrov@gmail.com>
2025-09-15 16:23:15 +0200

    optimized + static/dynamic fixes

--- src/furax/obs/operators/_beam_operator_mapspace.py
+++ src/furax/obs/operators/_beam_operator_mapspace.py
@@ -1,8 +1,7 @@
 import equinox
 import jax
 import jax.numpy as jnp
-from jaxtyping import Array, Float, Inexact, PyTree
-from scipy.sparse import load_npz
+from jaxtyping import Array, Inexact, PyTree
 
 from furax import AbstractLinearOperator, symmetric
 
@@ -14,45 +13,41 @@ class BeamOperatorMapspace(AbstractLinearOperator):
 
     Args:
         _in_structure (PyTree[jax.ShapeDtypeStruct]): Input structure of the operator.
-        path_to_file (str): Path to the file containing the sparse beam matrix in .npz format.
-        sparse_Beam_matrix (Float[Array, 'a b']): Sparse matrix representing the beam.
+        _indices (Array): Indices of the sparse matrix in CSR format.
+        _data (Array): Data of the sparse matrix in CSR format.
         _N_neighbours (int): Number of non-zero entries per row in the sparse matrix.
+        _n_rows (int): Number of rows in the sparse matrix.
     Returns:
         PyTree[Inexact[Array, ' _b']]: Output map after applying the beam.
     """
-    
-    _in_structure: PyTree[jax.ShapeDtypeStruct] = equinox.field(static=True)
-    sparse_Beam_matrix: Float[Array, 'a b'] = equinox.field(static=True)
-    _N_neighbours: int = equinox.field(static=True)
 
-    def __init__(self, in_structure: PyTree[jax.ShapeDtypeStruct], path_to_file: str) -> None:
+    _in_structure: PyTree[jax.ShapeDtypeStruct] = equinox.field(static=True)
+    _N_neighbours: int = equinox.field(static=False)
+    _indices: Array = equinox.field(static=False)
+    _data: Array = equinox.field(static=False)
+    _n_rows: int = equinox.field(static=False)
+
+    def __init__(self, in_structure: PyTree[jax.ShapeDtypeStruct], indices: Array, data: Array, N_neighbours: int, n_rows: int) -> None:
         self._in_structure = in_structure
-        self.sparse_Beam_matrix = load_npz(path_to_file)
-        self._N_neighbours = int(jnp.diff(self.sparse_Beam_matrix.indptr)[0])
+        self._indices = indices
+        self._data = data
+        self._N_neighbours = N_neighbours
+        self._n_rows = n_rows
 
     def mv(self, x: PyTree[Inexact[Array, ' _a']]) -> PyTree[Inexact[Array, ' _b']]: 
         def apply_sparse_matrix(stokes_comp):
-            # here comes in one Stokes parameter with shape [freq, npix]
-
+            # this function takes in one Stoke's parameter with shape [freq, npix]
+            
             def dot_product(mp):
-                # Calculate the dot product of the sparse matrix with one map mp
+                # Calculate the dot product of the sparse matrix with one frequency map
 
                 # I could take it out of here if it's the same beam for all frequency channels 
-                # and all Stokes parameters
-                indices = jnp.array(self.sparse_Beam_matrix.indices)
-                data = jnp.array(self.sparse_Beam_matrix.data)
-                n_rows = self.sparse_Beam_matrix.shape[0]
+                # and all Stokes parameters. self._N_neighbours could also be calculated here 
+                # if we have different number of neighbours per frequency channel 
+                all_indices = self._indices.reshape(self._n_rows, self._N_neighbours)
+                all_data = self._data.reshape(self._n_rows, self._N_neighbours)
 
-                # self._N_neighbours could be calculated here if we have different 
-                # number of neighbours per frequency channel 
-                all_indices = indices.reshape(n_rows, self._N_neighbours)
-                all_data = data.reshape(n_rows, self._N_neighbours)
-
-                def explicit_dot_product(row_data, row_indices):
-                    # here I operate on each pixel of map mp individually
-                    return jnp.sum(row_data * mp[row_indices])
-
-                return jax.vmap(explicit_dot_product)(all_data, all_indices) 
+                return jnp.sum(all_data * mp[all_indices], axis=1)
 
             return jax.vmap(dot_product)(jnp.atleast_2d(stokes_comp))
     

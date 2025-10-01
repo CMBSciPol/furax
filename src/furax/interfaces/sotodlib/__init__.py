5fd2ab251cf8e771470bbea4932cc9076fd84f1c
Pierre Chanial <chanial@apc.in2p3.fr>
2025-10-02 01:26:18 +0200

    SOTODLib reader

--- src/furax/interfaces/sotodlib/__init__.py
+++ src/furax/interfaces/sotodlib/__init__.py
@@ -1,5 +1,9 @@
+from .io import SOTODLibReader
 from .mapmaker import load_result
+from .observation import SOTODLibObservation
 
 __all__ = [
     'load_result',
+    'SOTODLibReader',
+    'SOTODLibObservation',
 ]

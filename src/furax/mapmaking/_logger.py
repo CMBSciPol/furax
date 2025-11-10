5db618dee5560fed0ec2cecbf7d3dde149532543
Wuhyun Sohn <sohn@apc.in2p3.fr>
2025-11-10 16:21:20 +0100

    Improve logging to work in various environments

--- src/furax/mapmaking/_logger.py
+++ src/furax/mapmaking/_logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 
 __all__ = [
     'logger',
@@ -7,3 +8,12 @@ __all__ = [
 
 logger = logging.getLogger('furax-mapmaking')
 logger.setLevel(os.getenv('LOGLEVEL', 'INFO').upper())
+
+# Add handler if none exists
+if not logger.handlers:
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)  # Handler accepts all levels
+    formatter = logging.Formatter('%(levelname)s [%(name)s]: %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False  # Don't propagate to root to avoid duplicate messages

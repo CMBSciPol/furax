7da833f19c30918ea36ea46835c946b0318a86d3
Simon Biquard <sbiquard+git@gmail.com>
2025-11-17 14:11:35 +0000

    do not use nperseg to convert noise model to Toeplitz operator

--- src/furax/mapmaking/noise.py
+++ src/furax/mapmaking/noise.py
@@ -169,20 +169,19 @@ class AtmosphericNoiseModel(NoiseModel):
         inverse fft of the noise psd, which is apodized and cut at given length
         """
 
-        nperseg: int = cast(int, kwargs.get('nperseg'))
         sample_rate: float = cast(float, kwargs.get('sample_rate'))
         correlation_length: int = cast(int, kwargs.get('correlation_length'))
         window = apodization_window(correlation_length)
 
-        freq = jnp.fft.rfftfreq(nperseg, 1 / sample_rate)
+        fft_size = SymmetricBandToeplitzOperator._get_default_fft_size(2 * correlation_length - 1)
+        freq = jnp.fft.rfftfreq(fft_size, 1 / sample_rate)
         eval_psd = self.psd(freq)
-        invntt = jnp.fft.irfft(1.0 / eval_psd, n=nperseg)[..., :correlation_length]
+        invntt = jnp.fft.irfft(1.0 / eval_psd, n=fft_size)[..., :correlation_length]
         inv_band = invntt * window
-        # padded_band = jnp.pad(inv_band, [(0, 0), (0, nperseg // 2 + 1 - inv_band.shape[-1])])
-        padded_band = jnp.pad(inv_band, [(0, 0), (0, nperseg + 1 - inv_band.shape[-1])])
+        padded_band = jnp.pad(inv_band, [(0, 0), (0, fft_size + 1 - inv_band.shape[-1])])
         symmetrised_band = jnp.concatenate([padded_band, padded_band[..., -2:0:-1]], axis=-1)
-        eff_inv_psd = jnp.fft.rfft(symmetrised_band, n=nperseg).real
-        new_band = jnp.fft.irfft(1.0 / eff_inv_psd, n=nperseg)[:, :correlation_length] * window
+        eff_inv_psd = jnp.fft.rfft(symmetrised_band, n=fft_size).real
+        new_band = jnp.fft.irfft(1.0 / eff_inv_psd, n=fft_size)[:, :correlation_length] * window
         return SymmetricBandToeplitzOperator(new_band, in_structure=in_structure)
 
     def inverse_operator(
@@ -192,14 +191,14 @@ class AtmosphericNoiseModel(NoiseModel):
         inverse fft of the noise psd, which is apodized and cut at given length
         """
 
-        nperseg: int = cast(int, kwargs.get('nperseg'))
         sample_rate: float = cast(float, kwargs.get('sample_rate'))
         correlation_length: int = cast(int, kwargs.get('correlation_length'))
 
-        freq = jnp.fft.rfftfreq(nperseg, 1 / sample_rate)
+        fft_size = SymmetricBandToeplitzOperator._get_default_fft_size(2 * correlation_length - 1)
+        freq = jnp.fft.rfftfreq(fft_size, 1 / sample_rate)
         eval_psd = self.psd(freq)
         inv_psd = jnp.where(eval_psd > 0, 1 / eval_psd, 0.0)
-        invntt = jnp.fft.irfft(inv_psd, n=nperseg)[..., :correlation_length]
+        invntt = jnp.fft.irfft(inv_psd, n=fft_size)[..., :correlation_length]
         window = apodization_window(correlation_length)
 
         return SymmetricBandToeplitzOperator(invntt * window, in_structure)

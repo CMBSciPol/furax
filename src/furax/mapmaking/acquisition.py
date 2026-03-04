0fb3ebf8bf0602219d6685218112b7690573a15b
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-03-04 11:04:43 +0000

    move build_acquisition_operator to new file

---
+++ src/furax/mapmaking/acquisition.py
@@ -0,0 +1,72 @@
+import jax.numpy as jnp
+from jaxtyping import Array, DTypeLike
+
+from furax import AbstractLinearOperator
+from furax.math.quaternion import to_gamma_angles
+from furax.obs import HWPOperator, LinearPolarizerOperator, QURotationOperator
+from furax.obs.landscapes import StokesLandscape
+
+from .pointing import PointingOperator
+
+__all__ = [
+    'build_acquisition_operator',
+]
+
+
+def build_acquisition_operator(
+    landscape: StokesLandscape,
+    boresight_quaternions: Array,
+    detector_quaternions: Array,
+    hwp_angles: Array | None = None,
+    *,
+    demodulated: bool = False,
+    pointing_on_the_fly: bool = True,
+    pointing_chunk_size: int = 16,
+    dtype: DTypeLike = jnp.float64,
+) -> AbstractLinearOperator:
+    """Build an acquisition operator for a single observation. Does not include masking."""
+    # The TOD shape
+    ndet = detector_quaternions.shape[0]
+    nsamp = boresight_quaternions.shape[0]
+    data_shape = (ndet, nsamp)
+
+    # Rotate into detector frame unless a HWP is present (even if demodulated)
+    # NB: for demodulated data we have to rotate the other way...
+    has_hwp = hwp_angles is not None or demodulated
+    pointing = PointingOperator.create(
+        landscape,
+        boresight_quaternions,
+        detector_quaternions,
+        chunk_size=pointing_chunk_size,
+        flip=demodulated,
+        frame='boresight' if has_hwp else 'detector',
+    )
+    if not pointing_on_the_fly:
+        pointing = pointing.as_expanded_operator()  # type: ignore[assignment]
+
+    # If there is no HWP, we just add a polarizer at the end
+    # NB: already in detector frame at this point
+    polarizer = LinearPolarizerOperator.create(
+        shape=data_shape,
+        dtype=dtype,
+        stokes=landscape.stokes,
+    )
+    if not has_hwp:
+        return polarizer @ pointing
+
+    # If there is a HWP, we need an additional rotation related to detector angle
+    gamma = to_gamma_angles(detector_quaternions)[:, None]
+    rot = QURotationOperator.create(data_shape, dtype, landscape.stokes, angles=gamma)
+
+    # In the demodulated case, there is no polarizer
+    if demodulated:
+        return rot.T @ pointing
+
+    # In the general case, we include polarizer and HWP
+    hwp = HWPOperator.create(
+        shape=data_shape,
+        dtype=dtype,
+        angles=hwp_angles,
+        stokes=landscape.stokes,
+    )
+    return polarizer @ rot.T @ hwp @ pointing
--- src/furax/mapmaking/mapmaker.py
+++ src/furax/mapmaking/mapmaker.py
@@ -36,7 +36,6 @@ from furax.core import (
     CompositionOperator,
     IndexOperator,
 )
-from furax.math.quaternion import to_gamma_angles
 from furax.obs.landscapes import HealpixLandscape, StokesLandscape, WCSLandscape
 from furax.obs.operators import HWPOperator, LinearPolarizerOperator, QURotationOperator
 from furax.obs.stokes import Stokes, StokesIQU, StokesPyTreeType, ValidStokesType
@@ -46,6 +45,7 @@ from . import templates
 from ._logger import logger as furax_logger
 from ._observation import AbstractGroundObservation, AbstractLazyObservation
 from ._reader import ObservationReader
+from .acquisition import build_acquisition_operator
 from .config import Landscapes, MapMakingConfig, Methods
 from .gap_filling import GapFillingOperator
 from .noise import AtmosphericNoiseModel, NoiseModel, WhiteNoiseModel
@@ -268,12 +268,12 @@ class MultiObservationMapMaker(Generic[T]):
         @jax.jit
         def get_acquisition(i: int) -> AbstractLinearOperator:
             data, _padding = reader.read(i)
-            return _build_acquisition_operator(
+            return build_acquisition_operator(
                 self.landscape,
-                boresight_quaternions=data['boresight_quaternions'],
-                detector_quaternions=data['detector_quaternions'],
+                data['boresight_quaternions'],
+                data['detector_quaternions'],
+                data.get('hwp_angles'),
                 demodulated=self.config.demodulated,
-                hwp_angles=data.get('hwp_angles'),
                 pointing_chunk_size=self.config.pointing_chunk_size,
                 pointing_on_the_fly=self.config.pointing_on_the_fly,
                 dtype=dtype,
@@ -487,65 +487,6 @@ def _build_landscape(config: MapMakingConfig) -> StokesLandscape:
     if config.landscape.type == Landscapes.WCS:
         raise NotImplementedError
     raise NotImplementedError
-
-
-def _build_acquisition_operator(
-    landscape: StokesLandscape,
-    boresight_quaternions: Array,
-    detector_quaternions: Array,
-    demodulated: bool,
-    hwp_angles: Array | None,
-    pointing_chunk_size: int,
-    pointing_on_the_fly: bool,
-    dtype: DTypeLike = jnp.float64,
-) -> AbstractLinearOperator:
-    """Build an acquisition operator for a single observation. Does not include masking."""
-    if not pointing_on_the_fly:
-        raise NotImplementedError
-
-    ndet = detector_quaternions.shape[0]
-    nsamp = boresight_quaternions.shape[0]
-    data_shape = (ndet, nsamp)
-
-    # Rotate into detector frame unless a HWP is present (even if demodulated)
-    # NB: for demodulated data we have to rotate the other way...
-    has_hwp = hwp_angles is not None or demodulated
-    pointing = PointingOperator.create(
-        landscape,
-        boresight_quaternions,
-        detector_quaternions,
-        chunk_size=pointing_chunk_size,
-        flip=demodulated,
-        frame='boresight' if has_hwp else 'detector',
-    )
-
-    # If there is no HWP, we just add a polarizer at the end
-    # NB: already in detector frame at this point
-    polarizer = LinearPolarizerOperator.create(
-        shape=data_shape,
-        dtype=dtype,
-        stokes=landscape.stokes,
-    )
-    if not has_hwp:
-        return polarizer @ pointing
-
-    # If there is a HWP, we need an additional rotation related to detector angle
-    gamma = to_gamma_angles(detector_quaternions)[:, None]
-    rot = QURotationOperator.create(data_shape, dtype, landscape.stokes, angles=gamma)
-
-    # In the demodulated case, there is no polarizer
-    if demodulated:
-        return rot.T @ pointing
-
-    # In the general case, we include polarizer and HWP
-    hwp = HWPOperator.create(
-        shape=data_shape,
-        dtype=dtype,
-        angles=hwp_angles,
-        stokes=landscape.stokes,
-    )
-    acquisition = polarizer @ rot.T @ hwp @ pointing
-    return acquisition.reduce()
 
 
 def _build_mask_projector(

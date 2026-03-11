46e43804902cba1a5be7ddd4272db724c2cc7cac
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-03-11 19:07:35 +0000

    low_rank wrapper and operator

--- src/furax/linalg/__init__.py
+++ src/furax/linalg/__init__.py
@@ -3,6 +3,7 @@ from ._eigvalsh import eigvalsh
 from ._lanczos import LanczosResult, lanczos_eigh, lanczos_tridiag
 from ._lobpcg import LOBPCGResult, lobpcg_standard
 from ._nystrom import NystromPreconditioner, NystromResult, randomized_nystrom
+from .low_rank import LowRankOperator, LowRankTerms, low_rank, low_rank_mv
 
 __all__ = [
     'cg',
@@ -16,4 +17,8 @@ __all__ = [
     'randomized_nystrom',
     'NystromResult',
     'NystromPreconditioner',
+    'LowRankTerms',
+    'low_rank',
+    'low_rank_mv',
+    'LowRankOperator',
 ]

801e878254df59f4b3035f1cddc757d62f81aa49
Pierre Chanial <chanial@apc.in2p3.fr>
2026-03-19 17:39:54 +0100

    comments wassim

--- paper/make.sh
+++ paper/make.sh
@@ -1,1 +1,1 @@
-docker run --rm --volume /home/chanial/work/scipol/furax/paper-joss:/data --user 1000:1000 --env JOURNAL=joss openjournals/inara
+docker run --rm --volume /home/chanial/work/scipol/furax/paper:/data --user 1000:1000 --env JOURNAL=joss openjournals/inara

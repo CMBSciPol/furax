721c2707aa572eec67d668c6481ac50198c99ddd
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-04-07 12:02:25 +0100

    KS method

--- src/furax/linalg/__init__.py
+++ src/furax/linalg/__init__.py
@@ -1,6 +1,6 @@
 from ._cg import CGResult, cg
 from ._eigvalsh import eigvalsh
-from ._lanczos import LanczosResult, lanczos_eigh, lanczos_tridiag
+from ._lanczos import LanczosResult, lanczos_eigh, lanczos_ks, lanczos_tridiag
 from ._lobpcg import LOBPCGResult, lobpcg_standard
 from ._nystrom import NystromPreconditioner, NystromResult, randomized_nystrom
 from .low_rank import LowRankOperator, LowRankTerms, low_rank, low_rank_mv
@@ -10,6 +10,7 @@ __all__ = [
     'CGResult',
     'eigvalsh',
     'lanczos_eigh',
+    'lanczos_ks',
     'lanczos_tridiag',
     'LanczosResult',
     'lobpcg_standard',

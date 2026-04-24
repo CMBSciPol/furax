cebfef3572e6e3ee7d58fb5055a5464323ef2ace
Wuhyun Sohn <sohn@apc.in2p3.fr>
2026-04-24 14:51:46 +0200

    Add a getter for HWP frequency in observations

--- tests/interfaces/sotodlib/test_observation.py
+++ tests/interfaces/sotodlib/test_observation.py
@@ -1,17 +1,19 @@
 from pathlib import Path
 
 import pytest
+from numpy.testing import assert_allclose
 
 from furax.interfaces.sotodlib import SOTODLibObservation
 
+FOLDER = Path(__file__).parents[2] / 'data/sotodlib'
+FILE = FOLDER / 'test_obs.h5'
+
 
 def test_from_file() -> None:
-    folder = Path(__file__).parents[2] / 'data/sotodlib'
-    file = folder / 'test_obs.h5'
     ndet = 2
     nsample = 1000
 
-    obs = SOTODLibObservation.from_file(file)
+    obs = SOTODLibObservation.from_file(FILE)
     assert obs.n_detectors == ndet
     assert obs.n_samples == nsample
 
@@ -19,3 +21,9 @@ def test_from_file() -> None:
 def test_from_file_missing() -> None:
     with pytest.raises(FileNotFoundError):
         SOTODLibObservation.from_file('non_existing_file.h5')
+
+
+def test_get_hwp_frequency() -> None:
+    obs = SOTODLibObservation.from_file(FILE)
+    freq = obs.get_hwp_frequency()
+    assert_allclose(float(freq), 2.0, rtol=1e-5)

34adf984dbb70355fb94edab18f62ec7c253a6e6
Simon Biquard <79218427+sbiquard@users.noreply.github.com>
2026-05-08 13:34:08 +0100

    Implement non-ideal HWP operator

--- src/furax/obs/operators/__init__.py
+++ src/furax/obs/operators/__init__.py
@@ -1,5 +1,5 @@
 from ._beam_operator import BeamOperator, BeamOperatorIQU
-from ._hwp import HWPOperator
+from ._hwp import HWPOperator, NonIdealHWPOperator, hwp_mueller_from_stack
 from ._polarizers import LinearPolarizerOperator
 from ._qu_rotations import QURotationOperator
 from ._seds import (
@@ -10,10 +10,13 @@ from ._seds import (
     NoiseDiagonalOperator,
     SynchrotronOperator,
 )
+from ._transfer_matrix import Material, Stack, mueller_matrix
 
 __all__ = [
     # _hwp
     'HWPOperator',
+    'NonIdealHWPOperator',
+    'hwp_mueller_from_stack',
     # _polarizers
     'LinearPolarizerOperator',
     # _qu_rotations
@@ -28,4 +31,8 @@ __all__ = [
     # _beam_operator
     'BeamOperator',
     'BeamOperatorIQU',
+    # _transfer_matrix
+    'Material',
+    'Stack',
+    'mueller_matrix',
 ]
